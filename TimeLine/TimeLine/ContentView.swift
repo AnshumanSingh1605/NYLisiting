3e05e1c7931491d26f69bd8ecb067cd33a63b8e3
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-19 19:24:03 +0530

    Adding Project TimeLine in the NYListing workspace.

---
+++ TimeLine/TimeLine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLine
+//
+//  Created by Anshuman on 19/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
