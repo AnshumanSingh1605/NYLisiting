5b958ab070c1e02095629291b5fb01c09c00c227
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-19 19:27:49 +0530

    Adding Endpoint project/framework for network communication.

---
+++ EndPoints/EndPoints/EndPoints.h
@@ -0,0 +1,19 @@
+//
+//  EndPoints.h
+//  EndPoints
+//
+//  Created by Anshuman on 19/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EndPoints.
+FOUNDATION_EXPORT double EndPointsVersionNumber;
+
+//! Project version string for EndPoints.
+FOUNDATION_EXPORT const unsigned char EndPointsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EndPoints/PublicHeader.h>
+
+
