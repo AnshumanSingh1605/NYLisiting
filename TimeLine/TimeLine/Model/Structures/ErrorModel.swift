2b40b09549bcb60d609d035de9d298b1a8a25037
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 00:34:45 +0530

    Adding codable structures to handle server response

---
+++ TimeLine/TimeLine/Model/Structures/ErrorModel.swift
@@ -0,0 +1,29 @@
+//
+//  ErrorModel.swift
+//  TimeLine
+//
+//  Created by Anshuman on 20/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+import Foundation
+
+/**
+
+{"fault":{"faultstring":"Invalid ApiKey","detail":{"errorcode":"oauth.v2.InvalidApiKey"}}}
+
+
+**/
+
+struct ErrorModel : Codable {
+    let fault : FaultModel
+}
+
+struct FaultModel : Codable {
+    let faultstring  : String
+    let detail : FaultDetailModel
+}
+
+struct FaultDetailModel : Codable {
+    let errorcode : String
+}
---
+++ TimeLine/TimeLine/Model/Structures/MediaModel.swift
@@ -0,0 +1,62 @@
+//
+//  MediaModel.swift
+//  TimeLine
+//
+//  Created by Anshuman on 20/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Media
+struct Media: Codable {
+    let type: MediaType
+    let subtype: Subtype
+    let caption, copyright: String
+    let approvedForSyndication: Int
+    let mediaMetadata: [MediaMetadatum]
+
+    enum CodingKeys: String, CodingKey {
+        case type, subtype, caption, copyright
+        case approvedForSyndication = "approved_for_syndication"
+        case mediaMetadata = "media-metadata"
+    }
+}
+
+// MARK: - MediaMetadatum
+struct MediaMetadatum: Codable {
+    let url: String
+    let format: Format
+    let height, width: Int
+}
+
+enum Format: String, Codable {
+    case mediumThreeByTwo210 = "mediumThreeByTwo210"
+    case mediumThreeByTwo440 = "mediumThreeByTwo440"
+    case standardThumbnail = "Standard Thumbnail"
+}
+
+enum Subtype: String, Codable {
+    case empty = ""
+    case photo = "photo"
+}
+
+enum MediaType: String, Codable {
+    case image = "image"
+}
+
+enum Source: String, Codable {
+    case newYorkTimes = "New York Times"
+}
+
+enum Subsection: String, Codable {
+    case empty = ""
+    case europe = "Europe"
+    case live = "Live"
+    case politics = "Politics"
+}
+
+enum ResultType: String, Codable {
+    case article = "Article"
+    case interactive = "Interactive"
+}
