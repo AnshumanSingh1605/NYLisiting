51c30856274342d5a6ca087c756dfe43bb2da367
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 13:54:16 +0530

    View to provide radio button fucntinality for the user to select

---
+++ TimeLine/TimeLine/Views/CustomViews/RadioButton.swift
@@ -0,0 +1,21 @@
+//
+//  RadioButton.swift
+//  TimeLine
+//
+//  Created by Anshuman on 20/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButton()
+    }
+}
