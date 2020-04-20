2b59107aea5b2d0893efe73334464671a6aebd95
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 13:50:48 +0530

    Added App Transport security settings

--- TimeLine/TimeLine/Launch/AppDelegate.swift
+++ TimeLine/TimeLine/Launch/AppDelegate.swift
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         peredictAppMode()
+        
+        UITableView.appearance().separatorColor = .clear
+        UITableView.appearance().backgroundColor = .clear
+        
+        UITableViewCell.appearance().backgroundColor = .clear
+        UITableViewCell.appearance().selectionStyle = .none
+        
         return true
     }
     
