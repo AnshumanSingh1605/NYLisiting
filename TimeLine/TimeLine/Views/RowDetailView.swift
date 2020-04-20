efb3ee0fd6fdcc9bc7f130a87bff72a471ad69d9
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 13:48:56 +0530

    Row detail to present description of the article.

---
+++ TimeLine/TimeLine/Views/RowDetailView.swift
@@ -0,0 +1,24 @@
+//
+//  RowDetailView.swift
+//  TimeLine
+//
+//  Created by Anshuman on 20/04/20.
+//  Copyright © 2020 Anshuman Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct RowDetailView: View {
+    
+    let model : ArticleModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct RowDetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RowDetailView()
+//    }
+//}
