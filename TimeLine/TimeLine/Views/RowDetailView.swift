0dfd3ade314671065511d8ebf85d66e391523ecc
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 18:05:36 +0530

    implemented article related details

--- TimeLine/TimeLine/Views/RowDetailView.swift
+++ TimeLine/TimeLine/Views/RowDetailView.swift
@@ -10,10 +10,73 @@ import SwiftUI
 
 struct RowDetailView: View {
     
+    @Binding var isTapped : Bool
+    
     let model : ArticleModel
     
+    private var imageUrl : String {
+        var arrMetaData : [MediaMetadatum] = []
+        for media in model.media {
+            arrMetaData += media.mediaMetadata
+        }
+        arrMetaData = arrMetaData.filter { $0.format != .standardThumbnail }
+        if arrMetaData.count > 0 {
+            return arrMetaData.first?.url ?? String()
+        } else {
+            arrMetaData = arrMetaData.filter { $0.format == .standardThumbnail }
+            return arrMetaData.first?.url ?? String()
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topLeading) {
+            
+            Constants.AppTheme.primary.value.edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 15) {
+                    ImageViewContainer(imageURL: imageUrl)
+                        .background(.tertiaryBackground)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding([.top,.horizontal], 10)
+                    
+                    Text(model.title)
+                        .fontWeight(.semibold)
+                        .font(.largeTitle)
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.leading)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.horizontal)
+                    HStack {
+                      Text(model.byline)
+                        .foregroundColor(.tertiary)
+                        .multilineTextAlignment(.leading)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .lineLimit(2)
+                        .font(.headline)
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Image.system(.calendar)
+                                .foregroundColor(.tertiary)
+                            
+                            Text(model.publishedDate)
+                                .foregroundColor(.tertiary)
+                                .font(.system(size: 15))
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .onDisappear(perform: {
+                self.isTapped = false
+            })
+        }
     }
 }
 
