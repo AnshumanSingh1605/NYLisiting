5fc33e6b454db7f72810ca7d81641b1838f41532
Anshuman Singh <anshuman.singh@algoworks.com>
2020-04-20 18:11:43 +0530

    assiging colours to the AppTheme properties

--- TimeLine/TimeLine/Model/Utilities/AppConstants/AppTheme.swift
+++ TimeLine/TimeLine/Model/Utilities/AppConstants/AppTheme.swift
@@ -11,18 +11,20 @@ import SwiftUI
 
 enum Constants {
     enum AppTheme {
-        case primary , secondary , tertiary , tertiaryBackground , backgroundGray ,quaternary , baseline , shadow
+        case primary , secondary
+        case tertiary , tertiaryBackground
+        case shadow
+        case base
         case custom(String)
+        
         var value : Color {
             switch self {
-            case .primary:                  return Constants.Colors.primary.value
-            case .secondary:                return Constants.Colors.secondary.value
-            case .tertiary :                return Constants.Colors.tertiary.value
-            case .quaternary :              return Constants.Colors.quaternary.value
-            case .baseline :                return Constants.Colors.baseline.value
-            case .tertiaryBackground :      return Constants.Colors.backgroundTertiary.value
-            case .backgroundGray :          return Constants.Colors.backgroundGray.value
-            case .shadow:                   return Constants.Colors.shadow.value
+            case .primary:                  return UserDefaultsManager.shared.appTheme == .light ? .white : .black
+            case .secondary:                return UserDefaultsManager.shared.appTheme == .light ? .black : .white
+            case .tertiary :                return UserDefaultsManager.shared.appTheme == .light ? .gray : .init(white: 0.5)
+            case .tertiaryBackground :      return UserDefaultsManager.shared.appTheme == .light ? .init(white: 0.8) : .init(white: 0.2)
+            case .shadow:                   return UserDefaultsManager.shared.appTheme == .light ? Color(white: 0.7) : Color(white: 0.3)
+            case .base:                     return UserDefaultsManager.shared.appTheme == .light ? Color(white: 0.9) : Color(white: 0.9)
             case .custom(let colorName):    return .init(colorName)
             }
         }
